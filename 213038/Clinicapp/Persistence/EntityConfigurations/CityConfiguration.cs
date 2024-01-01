c0e99bf35d2e8a3b31628d6aa67a5327641d6888
Muhammad Imran <213038@std.aumc.edu.pk>
2024-01-01 12:22:09 +0500

    first commit

---
+++ 213038/Clinicapp/Persistence/EntityConfigurations/CityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using ClinicManagement.Core.Models;
+
+namespace Clinicapp.Persistence.EntityConfigurations
+{
+    public class CityConfiguration : EntityTypeConfiguration<City>
+    {
+        public CityConfiguration()
+        {
+            Property(p => p.Name).IsRequired().HasMaxLength(255);
+        }
+    }
+}
